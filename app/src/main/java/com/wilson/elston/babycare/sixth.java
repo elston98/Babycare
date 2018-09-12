43b3b42ebb6623405d9146629817e0c715680a7e
Elston Corda <elston.corda98@gmail.com>
2018-09-12 21:16:22 +0530

    vaccination activity renamed

--- app/src/main/java/com/wilson/elston/babycare/sixth.java
+++ app/src/main/java/com/wilson/elston/babycare/sixth.java
@@ -1,15 +1,10 @@
 package com.wilson.elston.babycare;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class sixth extends AppCompatActivity {
@@ -17,7 +12,7 @@ public class sixth extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sixth);
+        setContentView(R.layout.vaccination);
 
     }
     @Override
