4ee0c535ce42509ee03639fe72b483cb116cdcb7
Elston Corda <elston.corda98@gmail.com>
2019-02-13 21:37:48 +0530

    Location Spinner added

--- app/src/main/java/com/wilson/elston/babycare/fourth.java
+++ app/src/main/java/com/wilson/elston/babycare/fourth.java
@@ -1,19 +1,28 @@
 package com.wilson.elston.babycare;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
 
 public class fourth extends AppCompatActivity {
 
+    Spinner spinner;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
@@ -27,6 +36,8 @@ public class fourth extends AppCompatActivity {
 
         ListView doclist=(ListView) findViewById(R.id.doc_list);
         doclist.setAdapter(adapter);
+
+
 
         doclist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -60,6 +71,27 @@ public class fourth extends AppCompatActivity {
             }
         });
 
+
+
+
     }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.spinner, menu);
+
+        MenuItem item = menu.findItem(R.id.spinner);
+        spinner = (Spinner) MenuItemCompat.getActionView(item);
+        spinner.setPopupBackgroundResource(R.color.pink);
+        spinner.setBackgroundResource(R.color.theme);
+        spinner.setDropDownWidth(200);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(fourth.this,R.array.spinner_item,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        return true;
+
+    }
+
 
 }
