6753ab14fdc5419c5fd08db622e20c5ef8788929
Elston Corda <elston.corda98@gmail.com>
2019-02-19 10:55:48 +0530

    Original app ready. Time to enhance in next commit.

--- app/src/main/java/com/wilson/elston/babycare/fourth.java
+++ app/src/main/java/com/wilson/elston/babycare/fourth.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -26,6 +27,7 @@ public class fourth extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
 
 
         String[] mobileArray = {"Pediatrician","Pediatric Dentist","Dermatologist","Allergist"};
